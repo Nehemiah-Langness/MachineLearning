b4684b35d94cc4658876a5a6efd65375dec965fd
Nehemiah-Langness <32148982+Nehemiah-Langness@users.noreply.github.com>
2018-03-28 19:57:14 -0500

    Cleaned Rule Properties

--- Domain/Condition.cs
+++ Domain/Condition.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Domain.Base;
+﻿using Domain.Base;
 using Domain.Contracts;
 
 namespace Domain
--- Domain/Rule.cs
+++ Domain/Rule.cs
@@ -11,11 +11,9 @@ namespace Domain
         public int Attempts { get; set; }
         public ICollection<Condition> Conditions { get; set; } = new HashSet<Condition>();
         public ICollection<Outcomes> Outcomes { get; set; } = new HashSet<Outcomes>();
-        public ResultStatus Result { get; set; }
 
-        IEnumerable<IKeyValue> IRule.Conditions => Conditions.Select(c => (IKeyValue) c);
-        IEnumerable<IKeyValue> IRule.Outcomes => Outcomes.Select(o => (IKeyValue)o);
-
+        IEnumerable<IKeyValue> IRule.Conditions => Conditions;
+        IEnumerable<IKeyValue> IRule.Outcomes => Outcomes;
         void IRule.AddResult(ResultStatus status)
         {
             Attempts++;
